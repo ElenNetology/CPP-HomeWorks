0ae153ac217ca2fb5347925b903bf555691e34a8
Elen test <lena.prudko@yandex.ru>
2022-11-28 22:48:51 +0400

    Tree

--- Lesson3_Class/Task3.1/Task3.1.cpp
+++ Lesson3_Class/Task3.1/Task3.1.cpp
@@ -6,87 +6,93 @@
 
 class calculytor
 {
-    double num1, num2;
-public:
-    double add(double num1, double num2)
+    int num1, num2;
+    void setnum1(int num_1)
     {
-     double add = num1 + num2;
-     std::cout <<"num1 + num2" << "=" << add << std::endl;
-     return num1 + num2;
+        num1 = num_1;
     }
-    double multiply(double num1, double num2)
+    void setnum2(int num_2)
     {
-        double multiply = num1 * num2;
-     std::cout << "num1 * num2" << "=" << multiply <<std::endl;
-     return num1 * num2;
+        num2 = num_2;
     }
-    double subtract_1_2(double num1, double num2)
+    bool set_num1(int num1)
     {
-        double subtract_1_2 = num1 - num2;
-     std::cout << "num1 - num2" << "=" << subtract_1_2 <<std::endl;
-     return num1 - num2;
-    }
-    double subtract_2_1(double num1, double num2)
-    {
-        double subtract_2_1 = num2 - num1;
-     std::cout <<"num2 - num1" << "=" << subtract_2_1 <<std::endl;
-     return num2 - num1 ;
-    }
-    double divide_1_2(double num1, double num2)
-    {
-        double divide_1_2 = num1 / num2;
-     std::cout << "num1 / num2" << "=" << divide_1_2 <<std::endl;
-     return num1 / num2;
-    }
-    double divide_2_1(double num1, double num2)
-    {
-        double divide_2_1 = num1 / num2;
-     std::cout << "num2 / num1" << "=" << divide_2_1 << std::endl;
-     return num2 / num1;
-    }
-    bool set_num1(double num1)
-    {
-        if (num1 != 0) {
-         this-> num1 = num1;
-         return true;
-        }else {
-            std::cout << "Неверный ввод!" << std::endl;
+        if ( num1 != 0) {
+            this->num1 = num1;
+            return true;
         }
-     }
-    bool set_num2(double num2)
+        else {
+            std::cout << "Неверный ввод!" << std::endl;
+            return false;
+        }
+    }
+    bool set_num2(int num2)
     {
         if (num2 != 0) {
-            this->num2 = num2;
-            return true;
+         this->num2 = num2;
+         return true;
         } else {
-            std::cout << "Неверный ввод!" << std::endl;
+          std::cout << "Неверный ввод!" << std::endl; 
+          return false;
+         }
+     }
+    public:
+    calculytor() : num1(0), num2(0)
+        {}
+        double add()
+        {
+            double adds = num1 + num2;
+            return adds;
         }
-    }
-};
+        double multiply()
+        {
+            double multp = num1 * num2;
+            return multp;
+        }
+        double subtract_1_2()
+        {
+            double subtr_1_2 = num1 - num2;
+            return subtr_1_2;
+        }
+        double subtract_2_1()
+        {
+            double subtr_2_1 = num2 - num1;
+            return subtr_2_1;
+        }
+        double divide_1_2()
+        {
+            double div_1_2 = num1 / num2;
+            return div_1_2;
+        }
+        double divide_2_1()
+        {
+            double div_2_1 = num2 / num1;
+            return div_2_1;
+        }
+ };
+    
+  
 
 int main(int argc, char** argv)
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "Russian");
-    double num1;
-    double num2;
+    calculytor calc;
+    int num_1, num_2;
     std::cout << "Введите num1: ";
-    std::cin >> num1;
+    std::cin >> num_1;
     std::cout<<std::endl;
     std::cout << "Введите num2: ";
-    std::cin >> num2;
+    std::cin >> num_2;
     std::cout << std::endl;
-    calculytor calc;
-        calc.add(num1,num2);
-        calc.multiply(num1, num2);
-        calc.subtract_1_2(num1, num2);
-        calc.subtract_2_1(num1, num2);
-        calc.divide_1_2(num1, num2);
-        calc.divide_2_1(num1, num2);
-        calc.set_num1(num1);
-        calc.set_num2(num2);
-
     
+    std::cout << "num1 + num2" << "=" << calc.add() << std::endl;
+    std::cout << "num1 * num2" << "=" << calc.multiply()<< std::endl;
+    std::cout << "num1 - num2" << "=" << calc.subtract_1_2() << std::endl;
+    std::cout << "num2 - num1" << "=" << calc.subtract_2_1() << std::endl;
+    std::cout << "num1 / num2" << "=" << calc.divide_1_2() << std::endl;
+    std::cout << "num2 / num1" << "=" << calc.divide_2_1() << std::endl;
+
     return 0;
 }
---
+++ Lesson3_Class/Task3.2/Task3.2.cpp
@@ -0,0 +1,73 @@
+﻿// Task3.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+class cCounter
+{
+    char otv;
+public:
+    cCounter(int _v) : counter(_v)
+    {
+        if ((_v < 0) || (_v > 999))
+            std::cout << "Неправильное число\n";
+    }
+        void add()
+    {
+        counter++;
+    }
+
+    void subtract()
+    {
+       counter--;
+    }
+
+    int get()
+    {
+        return counter;
+    }
+private:
+    int counter;
+};
+
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    int n;
+    //char otv;
+    //std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: "; std::cin >> otv;
+    std::cout << "Введите начальные показания счетчика: ";
+    std::cin >> n;
+
+    cCounter counter(n);
+
+    while (true)
+    {
+        char opt;
+        std::cout << "Введите +, -, = или x для выхода: ";
+        std::cin >> opt;
+
+        switch (opt)
+        {
+        case '+':
+            counter.add();
+            break;
+
+        case '-':
+            counter.subtract();
+            break;
+
+        case '=':
+            std::cout << "Показание счетчика " << counter.get() << "\n";
+            break;
+
+        case 'x':
+            return 0;
+
+        default:
+            std::cout << "До свидания!\n";
+        }
+    }
+
+    return 0;
+}
