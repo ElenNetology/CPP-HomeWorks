c7d70ac581338928edd8175091f600de3235b3e9
Elen test <lena.prudko@yandex.ru>
2022-12-13 23:36:14 +0400

    Task5.2i3

---
+++ Lesson5_NasP/Task5.2/Task5.2.cpp
@@ -0,0 +1,166 @@
+﻿// Task5.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+class Figure // класс фигура
+{
+protected:
+	std::string Figure;
+	bool FSide = false;
+	int A = 0;
+	int B = 0;
+	int C = 0;
+	int D = 0;
+	int AngA = 0;
+	int AngB = 0;
+	int AngC = 0;
+	int AngD = 0;
+public:
+	std::string getFigure() { return Figure; }
+	int getFSide() { return FSide; }
+	int getA() { return A; }
+	int getB() { return B; }
+	int getC() { return C; }
+	int getD() { return D; }
+	int getAngA() { return AngA; }
+	int getAngB() { return AngB; }
+	int getAngC() { return AngC; }
+	int getAngD() { return AngD; }
+};
+
+class Triangle : public Figure //Класс треугольник
+{
+public:
+	Triangle(int sideA, int sideB, int sideC, int angA, int angB, int angC)
+	{
+		Figure = "Треугольник";
+		A = sideA;
+		B = sideB;
+		C = sideC;
+		AngA = angA;
+		AngB = angB;
+		AngC = angC;
+	}
+};
+class rightTr : public Triangle
+{
+public:
+	rightTr(int sideA, int sideB, int sideC, int angA, int angB)
+		: Triangle(sideA, sideB, sideC, angA, angB, 90)
+	{
+		Triangle::Figure = "Прямоугольный треугольник";
+	};
+};
+class isoscelesTr : public Triangle
+{
+public:
+	isoscelesTr(int sideA, int sideB, int sideC, int angA, int angB)
+		: Triangle(sideA, sideB, sideC, angA, angB, angA)
+	{
+		Triangle::Figure = "Равнобедренный треугольник";
+	};
+};
+class equilateralTr : public Triangle
+{
+public:
+	equilateralTr(int sideA) : Triangle(sideA, sideA, sideA, 60, 60, 60)
+	{
+		Triangle::Figure = "Равносторонний треугольник";
+	};
+};
+class Quadrangle : public Figure // класс четырехугольник
+{
+public:
+	Quadrangle(int sideA, int sideB, int sideC, int sideD, int angA, int angB, int angC, int angD)
+	{
+		Figure = "Четырехугольник";
+		FSide = true;
+		A = sideA;
+		B = sideB;
+		C = sideC;
+		D = sideD;
+		AngA = angA;
+		AngB = angB;
+		AngC = angC;
+		AngD = angD;
+	}
+};
+class Rect : public Quadrangle
+{
+public:
+	Rect(int sideA, int sideB)
+		: Quadrangle(sideA, sideB, sideA, sideB, 90, 90, 90, 90)
+	{
+		Quadrangle::Figure = "Прямоугольник";
+	};
+};
+class Box : public Quadrangle
+{
+public:
+	Box(int sideA)
+		: Quadrangle(sideA, sideA, sideA, sideA, 90, 90, 90, 90)
+	{
+		Quadrangle::Figure = "Квадрат";
+	}
+};
+class Parallelogram : public Quadrangle
+{
+public:
+	Parallelogram(int sideA, int sideB, int angA, int angB)
+		: Quadrangle(sideA, sideB, sideA, sideB, angA, angB, angA, angB)
+	{
+		Quadrangle::Figure = "Параллелограмм";
+	}
+};
+class Romb : public Quadrangle
+{
+public:
+	Romb(int sideA, int angA, int angB)
+		: Quadrangle(sideA, sideA, sideA, sideA, angA, angB, angA, angB)
+	{
+		Quadrangle::Figure = "Ромб";
+	}
+};
+void printinfo(Figure& figure)
+{
+	std::cout << std::endl;
+	std::cout << figure.getFigure() << ":" << std::endl;
+	std::cout << "Стороны:";
+	std::cout << " a = " << figure.getA() << ", b = " << figure.getB() << ", с = " << figure.getC();
+	if (figure.getFSide())
+	{
+		std::cout << ", d = " << figure.getD() << std::endl;
+	}
+	else {
+		std::cout << std::endl;
+	}
+	std::cout << "Углы:";
+	std::cout << " А = " << figure.getAngA() << ", В = " << figure.getAngB() << ", С = " << figure.getAngC();
+	if (figure.getFSide()) { std::cout << ", D = " << figure.getAngD() << std::endl; }
+	else { std::cout << std::endl; }
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+	Triangle Tri1(15, 22, 38, 40, 50, 60);
+	rightTr rightTr1(10, 20, 30, 40, 50);
+	isoscelesTr isoscelesTr1(11, 110, 75, 40, 55);
+	equilateralTr equilateralTr1(111);
+	Quadrangle Quadrangle1(5, 8, 10, 41, 15, 35, 98, 110);
+	Rect Rect1(14, 24);
+	Box Box1(50);
+	Parallelogram Parallelogram1(40, 50, 25, 74);
+	Romb Romb1(14, 24, 34);
+	printinfo(Tri1);
+	printinfo(rightTr1);
+	printinfo(isoscelesTr1);
+	printinfo(equilateralTr1);
+	printinfo(Quadrangle1);
+	printinfo(Rect1);
+	printinfo(Box1);
+	printinfo(Parallelogram1);
+	printinfo(Romb1);
+}
---
+++ Lesson5_NasP/Task5.3/Task5.3.cpp
@@ -0,0 +1,352 @@
+﻿// Task5.3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+class Figure
+{
+protected:
+	std::string Figure = "Фигура";
+	bool checkF = false;
+	int NumberSide = 0;
+	int A = 0;
+	int B = 0;
+	int C = 0;
+	int D = 0;
+	int AngA = 0;
+	int AngB = 0;
+	int AngC = 0;
+	int AngD = 0;
+
+	virtual bool CheckF() //Проверка количества сторон 
+	{
+		if (NumberSide == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	};
+public:
+	std::string getFigure() { return Figure; }
+	int getA() { return A; }
+	int getB() { return B; }
+	int getC() { return C; }
+	int getD() { return D; }
+	int getAngA() { return AngA; }
+	int getAngB() { return AngB; }
+	int getAngC() { return AngC; }
+	int getAngD() { return AngD; }
+
+	virtual void printinfo()
+	{
+		std::cout << std::endl;
+		std::cout << Figure << ":" << std::endl;
+		if (CheckF())
+		{
+			std::cout << "Правильная" << std::endl;
+		}
+		else {
+			std::cout << "Неправильная" << std::endl;
+		}
+
+		std::cout << "Количество сторон: " << NumberSide;
+	}
+};
+class Triangle : public Figure //Класс треугольника
+{
+protected:
+	bool CheckF() override {
+		if ((AngA + AngB + AngC) == 180)
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	};
+public:
+
+	Triangle(int sideA, int sideB, int sideC, int angA, int angB, int angC)
+	{
+		Figure = "Треугольник";
+		A = sideA;
+		B = sideB;
+		C = sideC;
+		AngA = angA;
+		AngB = angB;
+		AngC = angC;
+		NumberSide = 3;
+		checkF = CheckF();
+	}
+	void printinfo() override
+	{
+		Figure::printinfo();
+		std::cout << "Стороны: ";
+		std::cout << " a = " << getA() << ", b = " << getB() << ", с = " << getC();
+		std::cout << std::endl;
+		std::cout << "Углы: ";
+		std::cout << " А = " << getAngA() << ", В = " << getAngB() << ", С = " << getAngC();
+		std::cout << std::endl;
+	}
+};
+class rightTr : public Triangle
+{
+protected:
+	bool CheckF() override
+	{
+		if (Triangle::CheckF() && (AngC == 90))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		};
+	};
+public:
+	rightTr(int sideA, int sideB, int sideC, int angA, int angB, int angC)
+		: Triangle(sideA, sideB, sideC, angA, angB, angC)
+	{
+		Triangle::Figure = "Прямоугольный треугольник";
+	};
+};
+class isoscelesTr : public Triangle
+{
+protected:
+	bool CheckF() override
+	{
+		if (Triangle::CheckF() && (A == C && AngA == AngC))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		};
+	};
+public:
+	isoscelesTr(int sideA, int sideB, int sideC, int angA, int angB, int angC)
+		: Triangle(sideA, sideB, sideC, angA, angB, angC)
+	{
+		Triangle::Figure = "Равнобедренный треугольник";
+	};
+};
+class equilateralTr : public Triangle
+{
+private:
+	bool Check_eqTr()
+	{
+		if ((A == B && B == C) && ((AngA == 60) && (AngA = 60) && (AngC = 60)))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		};
+	}
+public:
+	equilateralTr(int sideA, int sideB, int sideC, int angA, int angB, int angC)
+		: Triangle(sideA, sideB, sideC, angA, angB, angC)
+	{
+		Triangle::Figure = "Равносторонний треугольник";
+	};
+protected:
+	bool CheckF() override
+	{
+		if (Triangle::CheckF() && (Check_eqTr()))
+		{
+			return true;
+		}
+		else {
+			return false;
+		};
+	};
+};
+class Quadrangle : public Figure
+{
+protected:
+	bool CheckF() override
+	{
+		if ((AngA + AngB + AngC + AngD) == 360)
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	};
+	bool CheckSide()
+	{
+		if (A == C && B == D)
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	};
+	bool CheckSideABCD()
+	{
+		if (A == B && B == C && C == D)
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	};
+	bool CheckAng()
+	{
+		if (AngA == AngB == AngC == AngD == 90)
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	};
+	bool CheckAngACBD()
+	{
+		if (AngA == AngC && AngB == AngD)
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	};
+public:
+	Quadrangle(int sideA, int sideB, int sideC, int sideD, int angA, int angB, int angC, int angD)
+	{
+		Figure = "Четырехугольник";
+		NumberSide = 4;
+		A = sideA;
+		B = sideB;
+		C = sideC;
+		D = sideD;
+		AngA = angA;
+		AngB = angB;
+		AngC = angC;
+		AngD = angD;
+	}
+	void printinfo() override
+	{
+		Figure::printinfo();
+		std::cout << "Стороны: ";
+		std::cout << " a = " << getA() << ", b = " << getB() << ", с = " << getC();
+		std::cout << ", d = " << getD() << std::endl;
+		std::cout << "Углы: ";
+		std::cout << " А = " << getAngA() << ", В = " << getAngB() << ", С = " << getAngC();
+		std::cout << ", D = " << getAngD() << std::endl;
+	}
+};
+class Rect : public Quadrangle
+{
+protected:
+	bool CheckF() override
+	{
+		if (Quadrangle::CheckF() && Quadrangle::CheckSide() && Quadrangle::CheckAng())
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	};
+public:
+	Rect(int sideA, int sideB, int sideC, int sideD, int angA, int angB, int angC, int angD)
+		: Quadrangle(sideA, sideB, sideC, sideD, angA, angB, angC, angD)
+	{
+		Quadrangle::Figure = "Прямоугольник";
+	}
+};
+
+class Box : public Quadrangle
+{
+protected:
+	bool CheckF() override
+	{
+		if (Quadrangle::CheckF() && Quadrangle::CheckSide() && Quadrangle::CheckAng())
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+public:
+	Box(int sideA, int sideB, int sideC, int sideD, int angA, int angB, int angC, int angD)
+		: Quadrangle(sideA, sideB, sideC, sideD, angA, angB, angC, angD)
+	{
+		Quadrangle::Figure = "Квадрат";
+	}
+};
+class Parallelogram : public Quadrangle
+{
+protected:
+	bool CheckF() override
+	{
+		if (Quadrangle::CheckF() && Quadrangle::CheckSide() && Quadrangle::CheckAngACBD())
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+public:
+	Parallelogram(int sideA, int sideB, int sideC, int sideD, int angA, int angB, int angC, int angD)
+		: Quadrangle(sideA, sideB, sideC, sideD, angA, angB, angC, angD)
+	{
+		Quadrangle::Figure = "Параллелограмм";
+	}
+};
+class Romb : public Quadrangle
+{
+protected:
+	bool CheckF() override
+	{
+		if (Quadrangle::CheckF() && Quadrangle::CheckSide() && Quadrangle::CheckAngACBD())
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+public:
+	Romb(int sideA, int sideB, int sideC, int sideD, int angA, int angB, int angC, int angD)
+		: Quadrangle(sideA, sideB, sideC, sideD, angA, angB, angC, angD)
+	{
+		Quadrangle::Figure = "Ромб";
+	}
+};
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+	Figure Figure1;
+	Triangle Tri1(15, 22, 38, 40, 55, 34);
+	rightTr rightTr1(10, 10, 30, 60, 40, 80);
+	isoscelesTr isoscelesTr1(30, 30, 30, 60, 40, 80);
+	equilateralTr equilateralTr1(10, 10, 10, 60, 30, 90);
+	Quadrangle Quadrangle1(5, 8, 10, 41, 15, 35, 98, 50);
+	Rect Rect1(20, 10, 5, 10, 30, 30, 150, 150);
+	Box Box1(50, 50, 50, 50, 90, 90, 90, 90);
+	Parallelogram Parallelogram1(5, 8, 10, 41, 15, 35, 98, 50);
+	Romb Romb1(5, 8, 10, 10, 15, 35, 98, 50);
+	printinfo(Tri1);
+	printinfo(rightTr1);
+	printinfo(isoscelesTr1);
+	printinfo(equilateralTr1);
+	printinfo(Quadrangle1);
+	printinfo(Rect1);
+	printinfo(Box1);
+	printinfo(Parallelogram1);
+	printinfo(Romb1);
+}
