06d6495ddf44fddc1afe616e08b0127318410751
Elen test <lena.prudko@yandex.ru>
2023-01-16 19:59:30 +0400

    Pravka

--- Lesson8_Pr_Macr/Task8.1/Task8.1.cpp
+++ Lesson8_Pr_Macr/Task8.1/Task8.1.cpp
@@ -1,5 +1,10 @@
 ﻿#include <iostream>
 
+ double Add(double num1 =0, double num2 = 0)
+    {
+        return num1 + num2;
+    }
+
 #define MODE 2
 #if !defined MODE
 #error "сообщение о необходимости определить MODE"
@@ -11,10 +16,6 @@ int main()
     system("chcp 1251");
 
  #if MODE == 1
-  double Add(double num1 =0, double num2 = 0);
-    {
-        return num1 + num2;
-    }
     std::cout << std::endl << "Работаю в боевом режиме" << std::endl;
     double num1 = 0;
     double num2 = 0;
@@ -23,12 +24,11 @@ int main()
     std::cout << "Введите число 2: "; std::cin >> num2;
     std::cout << std::endl;
     std::cout << std::endl << "Результат сложения: " << Add(num1, num2) << std::endl;
-#endif
 
-    #if MODE == 0
+ #elif MODE == 0
     std::cout << std::endl << "Работаю в режиме тренировки" << std::endl;
-    #else 
-        std::cout << std::endl << "Неизвестный режим. Завершение работы" << std::endl;
+ #else 
+    std::cout << std::endl << "Неизвестный режим. Завершение работы" << std::endl;
  #endif
  
 
