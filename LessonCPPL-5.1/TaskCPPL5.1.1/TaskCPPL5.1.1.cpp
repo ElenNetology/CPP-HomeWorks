630bbf5fec7879c1730001f2dfc4dc9ebf3c7660
Elen test <lena.prudko@yandex.ru>
2023-03-14 22:13:36 +0400

    LessonCPPL5.1

---
+++ LessonCPPL-5.1/TaskCPPL5.1.1/TaskCPPL5.1.1.cpp
@@ -0,0 +1,24 @@
+﻿// TaskCPPL5.1.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <algorithm> 
+#include <iostream> 
+#include <vector>
+
+    int main()
+    {
+        setlocale(LC_ALL, "Russian");
+        std::vector < int > v{ 4, 7, 9, 14, 12 };
+
+        auto print = [](const  int& n) {  std::cout << n << ' ';  };
+
+        std::cout << "Входные данные: \t ";
+        std::for_each(v.cbegin(), v.cend(), print); std::cout << ' \n ';
+
+        std::for_each(v.begin(), v.end(), [](int& n) { n*3; });
+
+        std::cout << "\nВыходные данные:\t ";
+        std::for_each(v.cbegin(), v.cend(), print); std::cout << ' \n ';
+
+}
+
