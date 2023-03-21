ef06dac1686cf640f12c572d750e1a81bb8750ec
Elen test <lena.prudko@yandex.ru>
2023-03-21 21:16:30 +0400

    Kompil

--- LessonCPPL-2/Task2.1/Task2.1.cpp
+++ LessonCPPL-2/Task2.1/Task2.1.cpp
@@ -4,28 +4,25 @@
 #include <iostream>
 using namespace std;
 
-struct point {
+struct Point {
+	Point(double x, double y) : m_x(x), m_y(y) {}
 	double m_x;
 	double m_y;
-};
-point (double x, double y){
-		x = m_x;
-		m_y = y;
-	
+
 };
 
-void print_point(const point& point) {
+void print_point(const Point& point) {
 	std::cout << "x:" << point.m_x << ", y: "
 		<< point.m_y << std::endl;
 }
 
-int Main()
+int main()
 {
 	int i;
 	
 	for (i = 0; i < 5; i++)
 	{
-		point my_point(i, 2 * i);
+		Point my_point(i, 2 * i);
 		print_point(my_point);
 	}
 	return 0;
