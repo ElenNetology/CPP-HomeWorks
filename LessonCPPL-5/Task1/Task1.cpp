33565c06d89ae6fd1cdd9c39434f830d24daf360
Elen test <lena.prudko@yandex.ru>
2023-04-01 22:49:03 +0400

    Task5

---
+++ LessonCPPL-5/Task1/Task1.cpp
@@ -0,0 +1,23 @@
+﻿// Task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include<iostream>
+
+template<typename E>
+E square(E x)
+{
+	return x * x;
+}
+
+template<typename T>
+void print(T x, T(*fun)(T))
+{
+	std::cout << fun(x) << std::endl;
+}
+
+int main()
+{
+	int x{ 5 };
+	print(x, square<int>);
+	return 0;
+}
